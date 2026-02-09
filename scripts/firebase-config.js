9c4a899e8a2bba26f9b02fd26c87e6039db184a1
Mani Aggarwal <mani.ag2006@gmail.com>
2026-02-09 09:18:16 +0530

    first commit

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -1,10 +1,2 @@
 // Firebase configuration exported for other modules to use
-export const firebaseConfig = {
-  apiKey: "AIzaSyAWJSYX-BiIMGrM9V5cnDPFzoMtMD-xxm4",
-  authDomain: "smart-canteen-643f4.firebaseapp.com",
-  projectId: "smart-canteen-643f4",
-  storageBucket: "smart-canteen-643f4.firebasestorage.app",
-  messagingSenderId: "153105733569",
-  appId: "1:153105733569:web:e49dd04c4de05032f0c140",
-  measurementId: "G-HG7DTRN44N"
-};
+
